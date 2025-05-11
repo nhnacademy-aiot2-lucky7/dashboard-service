fdc5456773b86a9e4534edb8d9c26c260bce523f
Kimsongbeen <ksb10266@naver.com>
2025-05-11 18:26:25 +0900

    applicationtest수정

--- src/test/java/com/nhnacademy/dashboard/DashboardApplicationTests.java
+++ src/test/java/com/nhnacademy/dashboard/DashboardApplicationTests.java
@@ -1,19 +1,13 @@
 package com.nhnacademy.dashboard;
 
-import com.nhnacademy.dashboard.api.GrafanaApi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class DashboardApplicationTests {
 
-	@MockitoBean
-	GrafanaApi grafanaApi;
-
 	@Test
 	void contextLoads() {
-		throw new UnsupportedOperationException("Test not yet implemented.");
 	}
 
 }
