25c4674b732955d9a89810732b4136739dd9f9e5
Kimsongbeen <ksb10266@naver.com>
2025-05-22 17:16:08 +0900

    생성자추가

--- src/main/java/com/nhnacademy/dashboard/dto/panel/ReadPanelRequest.java
+++ src/main/java/com/nhnacademy/dashboard/dto/panel/ReadPanelRequest.java
@@ -2,9 +2,11 @@ package com.nhnacademy.dashboard.dto.panel;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReadPanelRequest {
 
     /**
