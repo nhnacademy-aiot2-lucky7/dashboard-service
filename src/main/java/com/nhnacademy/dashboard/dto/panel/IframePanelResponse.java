a6f9263292a4640521118e80dd1ec82cf7985a0f
HwangSlater <doorchan23@naver.com>
2025-06-06 20:32:39 +0900

    .

--- src/main/java/com/nhnacademy/dashboard/dto/panel/IframePanelResponse.java
+++ src/main/java/com/nhnacademy/dashboard/dto/panel/IframePanelResponse.java
@@ -13,28 +13,34 @@ public class IframePanelResponse {
     private int panelId;
     private long now;
     private long from;
+    private long w;
+    private long h;
 
-    private IframePanelResponse(String dashboardUid, String dashboardTitle, int panelId, long from) {
+    private IframePanelResponse(String dashboardUid, String dashboardTitle, int panelId, long from, long w, long h) {
         this.dashboardUid = dashboardUid;
         this.dashboardTitle = dashboardTitle;
         this.panelId = panelId;
         this.now = System.currentTimeMillis();
         this.from = now - from;
+        this.w = w;
+        this.h = h;
     }
 
-    private IframePanelResponse(String dashboardUid, String dashboardTitle, int panelId) {
+    private IframePanelResponse(String dashboardUid, String dashboardTitle, int panelId, long w, long h) {
         this.dashboardUid = dashboardUid;
         this.dashboardTitle = dashboardTitle;
         this.panelId = panelId;
         this.now = System.currentTimeMillis();
         this.from = now - (1000L * 60 * 60); // 기본값: 1시간 전
+        this.w = w;
+        this.h = h;
     }
 
     public static IframePanelResponse ofNewIframeResponse(String uid, String dashboardTitle, int panelId, long from) {
-        return new IframePanelResponse(uid, dashboardTitle, panelId, from);
+        return new IframePanelResponse(uid, dashboardTitle, panelId, from, 0, 0);
     }
 
-    public static IframePanelResponse ofNewIframeResponse(String uid, String dashboardTitle, int panelId) {
-        return new IframePanelResponse(uid, dashboardTitle, panelId);
+    public static IframePanelResponse ofNewIframeResponse(String uid, String dashboardTitle, int panelId, long w, long h) {
+        return new IframePanelResponse(uid, dashboardTitle, panelId, w, h);
     }
 }
